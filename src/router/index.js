915d27a8e875d5131d3baef643aa99c7e5cd071a
BinishaJ <bnsajoc@gmail.com>
2024-06-21 15:36:16 +0545

    Not found route added

--- src/router/index.js
+++ src/router/index.js
@@ -16,117 +16,124 @@ import {
 } from "../views";
 import { useUserStore } from "../stores/user";
 
+const routes = [
+  {
+    path: "/",
+    alias: "/login",
+    name: "login",
+    component: LoginView,
+    meta: { requiresNavbar: false, requiresAuth: false },
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: RegisterView,
+    meta: { requiresNavbar: false, requiresAuth: false },
+  },
+  {
+    path: "/home",
+    name: "home",
+    component: DashboardView,
+    meta: {
+      requiresNavbar: true,
+      requiresAuth: true,
+      requiredRoles: [1, 2, 3],
+    },
+  },
+  {
+    path: "/users",
+    meta: {
+      requiresNavbar: true,
+      requiresAuth: true,
+      requiredRoles: [1],
+    },
+    children: [
+      {
+        path: "",
+        component: UsersView,
+      },
+      {
+        path: "add",
+        component: AddUserView,
+      },
+      {
+        path: ":id/edit",
+        component: EditUserView,
+      },
+    ],
+  },
+  {
+    path: "/artists",
+    meta: { requiresNavbar: true, requiresAuth: true },
+    children: [
+      {
+        path: "",
+        component: ArtistsView,
+        meta: { requiredRoles: [1, 2] },
+      },
+      {
+        path: "add",
+        component: AddArtistView,
+        meta: { requiredRoles: [2] },
+      },
+      {
+        path: ":id/edit",
+        component: EditArtistView,
+        meta: { requiredRoles: [2] },
+      },
+      {
+        path: ":id/songs",
+        children: [
+          {
+            path: "",
+            component: SongsView,
+            meta: { requiredRoles: [1, 2] },
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: "/songs",
+    meta: {
+      requiresNavbar: true,
+      requiresAuth: true,
+      requiredRoles: [3],
+    },
+    children: [
+      {
+        path: "",
+        component: SongsView,
+        meta: { requiredRoles: [3] },
+      },
+      {
+        path: "add",
+        component: AddSongView,
+        meta: { requiredRoles: [3] },
+      },
+
+      {
+        path: ":id/edit",
+        component: EditSongView,
+        meta: { requiredRoles: [3] },
+      },
+    ],
+  },
+  {
+    path: "/logout",
+    name: "logout",
+    component: LogoutView,
+    meta: { requiresNavbar: false, requiresAuth: true },
+  },
+  {
+    path: "/:catchAll(.*)*",
+    redirect: { name: "home" },
+  },
+];
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "login",
-      component: LoginView,
-      meta: { requiresNavbar: false, requiresAuth: false },
-    },
-    {
-      path: "/register",
-      name: "register",
-      component: RegisterView,
-      meta: { requiresNavbar: false, requiresAuth: false },
-    },
-    {
-      path: "/home",
-      name: "home",
-      component: DashboardView,
-      meta: {
-        requiresNavbar: true,
-        requiresAuth: true,
-        requiredRoles: [1, 2, 3],
-      },
-    },
-    {
-      path: "/users",
-      meta: {
-        requiresNavbar: true,
-        requiresAuth: true,
-        requiredRoles: [1],
-      },
-      children: [
-        {
-          path: "",
-          component: UsersView,
-        },
-        {
-          path: "add",
-          component: AddUserView,
-        },
-        {
-          path: ":id/edit",
-          component: EditUserView,
-        },
-      ],
-    },
-    {
-      path: "/artists",
-      meta: { requiresNavbar: true, requiresAuth: true },
-      children: [
-        {
-          path: "",
-          component: ArtistsView,
-          meta: { requiredRoles: [1, 2] },
-        },
-        {
-          path: "add",
-          component: AddArtistView,
-          meta: { requiredRoles: [2] },
-        },
-        {
-          path: ":id/edit",
-          component: EditArtistView,
-          meta: { requiredRoles: [2] },
-        },
-        {
-          path: ":id/songs",
-          children: [
-            {
-              path: "",
-              component: SongsView,
-              meta: { requiredRoles: [1, 2] },
-            },
-          ],
-        },
-      ],
-    },
-    {
-      path: "/songs",
-      meta: {
-        requiresNavbar: true,
-        requiresAuth: true,
-        requiredRoles: [3],
-      },
-      children: [
-        {
-          path: "",
-          component: SongsView,
-          meta: { requiredRoles: [3] },
-        },
-        {
-          path: "add",
-          component: AddSongView,
-          meta: { requiredRoles: [3] },
-        },
-
-        {
-          path: ":id/edit",
-          component: EditSongView,
-          meta: { requiredRoles: [3] },
-        },
-      ],
-    },
-    {
-      path: "/logout",
-      name: "logout",
-      component: LogoutView,
-      meta: { requiresNavbar: false, requiresAuth: true },
-    },
-  ],
+  routes,
 });
 
 router.beforeEach((to, from, next) => {
